acdf6f959d05ca0ce68e12047a336e1189351d33
N1iKK <86393043+N1iKK@users.noreply.github.com>
2021-08-06 17:57:43 -0500

    Min corrections

--- Taller - Algoritmos de Cadenas en C/Main.c
+++ Taller - Algoritmos de Cadenas en C/Main.c
@@ -9,7 +9,7 @@ void main()
 	int op;
 	do
 	{
-		printf("\n_____Menu_____\n1.Convertir en nombre Propio el contenido del a cadena.\n2.Contar el número de veces que existe una palabra en una cadena.\n3.Enctriptar cadena.\n4.Desencriptar.\n5.Llenar caracteres por Izquierda o por derecha.\n6.Borrar caracteres de una cadena.\n7.Intersección.\n8.Diferencia entre dos cadenas.\n9.Borrar caracteres Izquierda o Derecha.\n10.Validar correo electronico.\n11.Salir\n>>");
+		printf("\n_____Menu_____\n1.Convertir en nombre Propio el contenido del a cadena.\n2.Contar el numero de veces que existe una palabra en una cadena.\n3.Enctriptar cadena.\n4.Desencriptar.\n5.Llenar caracteres por Izquierda o por derecha.\n6.Borrar caracteres de una cadena.\n7.Interseccion.\n8.Diferencia entre dos cadenas.\n9.Borrar caracteres Izquierda o Derecha.\n10.Validar correo electronico.\n11.Salir\n>>");
 		scanf_s("%d", &op);
 
 		switch (op)
@@ -17,7 +17,7 @@ void main()
 		case 1:
 			//Convertir en nombre Propio el contenido de la cadena
 
-			printf("Digite una palabra\n");
+			printf("Digite una palabra\n>>");
 			
 			char cadena[200];
 			char* p;
@@ -26,7 +26,7 @@ void main()
 			gets(cadena);
 			gets(p);
 
-			printf("%s\n", p);
+			//printf("%s\n", p);
 			
 			int i=1;
 			while (*p != '\0') {
@@ -44,13 +44,11 @@ void main()
 					*p = *p - 'a' + 'A';
 					i = 0;
 				}
-			printf("%s\n", p);
+				//printf("%s\n", p);
 				p++;
 			}
 
 			printf("La cadena queda: \"%s\" \n", cadena);
-			
-
 			
 			break;
 		case 2:
