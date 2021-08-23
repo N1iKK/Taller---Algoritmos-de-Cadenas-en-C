4c069d12a9de0d32bca7e9f259a3d06a3dd8e43c
Leff-o <86246797+Leff-o@users.noreply.github.com>
2021-08-23 10:11:26 -0500

    Corrección de sangría del código, debido a que tenía algunas llaves en posiciones erroneas.

--- Taller - Algoritmos de Cadenas en C/Main.c
+++ Taller - Algoritmos de Cadenas en C/Main.c
@@ -3,131 +3,113 @@
 #include <string.h>
 
 //Main menu
-void main()
-{
-	char cadena[200];
-	char* p;
-	char* tok;
-	char* next_tok1;
-	int op, cont;
-	do
-	{
-		printf("\n_____Menu_____\n1.Convertir en nombre Propio el contenido del a cadena.\n2.Contar el numero de veces que existe una palabra en una cadena.\n3.Enctriptar cadena.\n4.Desencriptar.\n5.Llenar caracteres por Izquierda o por derecha.\n6.Borrar caracteres de una cadena.\n7.Interseccion.\n8.Diferencia entre dos cadenas.\n9.Borrar caracteres Izquierda o Derecha.\n10.Validar correo electronico.\n11.Salir\n>>");
-		scanf_s("%d", &op);
+void main() {
+    char cadena[200];
+    char *p;
+    char *tok;
+    char *next_tok1;
+    int op, cont;
+    do {
+        printf("\n_____Menu_____\n1.Convertir en nombre Propio el contenido del a cadena.\n2.Contar el numero de veces que existe una palabra en una cadena.\n3.Enctriptar cadena.\n4.Desencriptar.\n5.Llenar caracteres por Izquierda o por derecha.\n6.Borrar caracteres de una cadena.\n7.Interseccion.\n8.Diferencia entre dos cadenas.\n9.Borrar caracteres Izquierda o Derecha.\n10.Validar correo electronico.\n11.Salir\n>>");
+        scanf_s("%d", &op);
 
-		switch (op)
-		{
-		case 1:
-			//Convertir en nombre Propio el contenido de la cadena
+        switch (op) {
+            case 1:
+                //Convertir en nombre Propio el contenido de la cadena
 
-			printf("Digite una palabra\n>>");
-			
-			//char cadena[200];
-			//char* p;
-			p = cadena;
+                printf("Digite una palabra\n>>");
 
-			gets(cadena);
-			gets(p);
+                //char cadena[200];
+                //char* p;
+                p = cadena;
 
-			//printf("%s\n", p);
-			
-			int i=1;
-			while (*p != '\0') {
+                gets(cadena);
+                gets(p);
 
-				if (*p == ' ')
-				{
-					i = 1;
-				}
-				else if (*p >= 'A' && *p <= 'Z')
-				{
-					i = 0;
-				}
-				else if (*p >= 'a' && *p <= 'z' && i==1)
-				{
-					*p = *p - 'a' + 'A';
-					i = 0;
-				}
-				//printf("%s\n", p);
-				p++;
-			}
+                //printf("%s\n", p);
 
-			printf("La cadena queda: \"%s\" \n", cadena);
-			
-			break;
-		case 2:
-			//Contar el número de veces que existe una palabra en una cadena
+                int i = 1;
+                while (*p != '\0') {
 
-			printf("Ingrese un texto (Maximo 200 letras).\n>>");
+                    if (*p == ' ') {
+                        i = 1;
+                    } else if (*p >= 'A' && *p <= 'Z') {
+                        i = 0;
+                    } else if (*p >= 'a' && *p <= 'z' && i == 1) {
+                        *p = *p - 'a' + 'A';
+                        i = 0;
+                    }
+                    //printf("%s\n", p);
+                    p++;
+                }
 
-			//char* tok;
-			//char* next_tok1;
-			cont = 0;
-			char palabra[20];
+                printf("La cadena queda: \"%s\" \n", cadena);
 
-			gets(cadena);
-			gets(cadena);
-			printf("Ingrese una palabra a buscar dentro del texto.\n>>");
+                break;
+            case 2:
+                //Contar el número de veces que existe una palabra en una cadena
 
-			gets(palabra);
+                printf("Ingrese un texto (Maximo 200 letras).\n>>");
 
-			tok = strtok_s(cadena, " \,\.\:\;", &next_tok1);
-			
-			while (tok != NULL)
-			{
-				if (_stricmp(tok, palabra)==0) 
-				{
-					cont++;
-				}
-				tok = strtok_s(NULL, " \,\.\:\;", &next_tok1);
-			}
+                //char* tok;
+                //char* next_tok1;
+                cont = 0;
+                char palabra[20];
 
-			printf("La palabra (%s) se encontro (%i) veces\n", palabra, cont);
+                gets(cadena);
+                gets(cadena);
+                printf("Ingrese una palabra a buscar dentro del texto.\n>>");
 
-			break;
-		case 3:
-			//Enctriptar cadena usando código ascii
+                gets(palabra);
 
-			printf("Ingrese el texto que quiere encriptar\n>>");
+                tok = strtok_s(cadena, " \,\.\:\;", &next_tok1);
 
-			gets(cadena);
-			gets(cadena);
+                while (tok != NULL) {
+                    if (_stricmp(tok, palabra) == 0) {
+                        cont++;
+                    }
+                    tok = strtok_s(NULL, " \,\.\:\;", &next_tok1);
+                }
 
-			for (int i = 0; i < 200 ; i++)
-			{
-				
-				if (cadena[i] == -52)
-				{
-					i++;
-				}
-				else
-				{
-					//printf("%c\n", cripText[i]);
-					printf("%c", cadena[i]+1);
-				}
-			}
+                printf("La palabra (%s) se encontro (%i) veces\n", palabra, cont);
 
-			break;
-		case 4:
-			//Desencriptar
+                break;
+            case 3:
+                //Enctriptar cadena usando código ascii
 
-			printf("Ingrese el texto que quiere desencriptar\n>>");
+                printf("Ingrese el texto que quiere encriptar\n>>");
 
-			gets(cadena);
-			gets(cadena);
+                gets(cadena);
+                gets(cadena);
 
-			for (int i = 0; i < 200; i++)
-			{
+                for (int i = 0; i < 200; i++) {
 
-				if (cadena[i] == -52)
-				{
-					i++;
-				}
-				else
-				{
-					//printf("%c\n", cripText[i]);
-					printf("%c", cadena[i] - 1);
-				}
-			}
+                    if (cadena[i] == -52) {
+                        i++;
+                    } else {
+                        //printf("%c\n", cripText[i]);
+                        printf("%c", cadena[i] + 1);
+                    }
+                }
+
+                break;
+            case 4:
+                //Desencriptar
+
+                printf("Ingrese el texto que quiere desencriptar\n>>");
+
+                gets(cadena);
+                gets(cadena);
+
+                for (int i = 0; i < 200; i++) {
+
+                    if (cadena[i] == -52) {
+                        i++;
+                    } else {
+                        //printf("%c\n", cripText[i]);
+                        printf("%c", cadena[i] - 1);
+                    }
+                }
 
                 break;
             case 5: {
@@ -136,45 +118,43 @@ void main()
                 gets(cadena);
                 int numRepe;
                 printf("ingrese la cantidad de veces\n>>");
-                if(scanf_s("%i",&numRepe) !=1) {
+                if (scanf_s("%i", &numRepe) != 1) {
                     printf("el valor debe ser un entero");
-                }else if(numRepe<0){
+                } else if (numRepe < 0) {
                     printf("Error!!!, no puede ingresar cantidades negativas");
-                }
-                else{
+                } else {
                     char variableCaracter[numRepe];
-                        printf("ingrese el caracter que quiere añadir\n>>");
+                    printf("ingrese el caracter que quiere añadir\n>>");
 
-                        gets(variableCaracter);
-                        gets(variableCaracter);
+                    gets(variableCaracter);
+                    gets(variableCaracter);
 
+                    printf("ingrese 1 para añadir por derecha\ningrese 2 para añadir por la izquierda\n>>");
+                    int op;
+                    scanf("%d", &op);
 
+                    if (op != 1 && op != 2) {
+                        printf("Error!!, Valor fuera de parámetro");
+                    }
 
-                        printf("ingrese 1 para añadir por derecha\ningrese 2 para añadir por la izquierda\n>>");
-                        int op;
-                        scanf("%d",&op);
-
-                        if(op!=1 && op!=2){
-                            printf("Error!!, Valor fuera de parámetro");
+                    if (op == 1) {
+                        for (int i = 0; i < numRepe; ++i) {
+                            strcat(cadena, variableCaracter);
                         }
+                        printf("La cadena queda:\n");
+                        printf("%s.\n", cadena);
 
-                        if(op==1){
-                            for (int i = 0; i < numRepe; ++i) {
-                                strcat(cadena, variableCaracter);
-                            }
-                            printf("La cadena queda:\n");
-                            printf("%s.\n", cadena);
-
-                        }if(op==2){
-                            printf("La cadena queda:\n");
-                            for (int i = 0; i < numRepe; ++i) {
-                                printf(variableCaracter);
-                                //strcat(cadena, variableCaracter);
-                            }
-                            printf(cadena);
+                    }
+                    if (op == 2) {
+                        printf("La cadena queda:\n");
+                        for (int i = 0; i < numRepe; ++i) {
+                            printf(variableCaracter);
+                            //strcat(cadena, variableCaracter);
                         }
+                        printf(cadena);
                     }
                 }
+            }
                 break;
             case 6:
 
@@ -185,18 +165,18 @@ void main()
                 gets(cadena);
                 int longText = strlen(cadena);
                 printf("Ingrese el caracter que quiere borrar");
-                scanf_s("%c",&borrar);
-                int contador=0;
+                scanf_s("%c", &borrar);
+                int contador = 0;
 
                 for (int i = 0; i < longText; ++i) {
-                    if (cadena[i] != borrar){
-                        newText[contador]= cadena[i];
+                    if (cadena[i] != borrar) {
+                        newText[contador] = cadena[i];
                         contador++;
                     }
                 }
                 newText[contador] = '\0';
 
-                printf("%s\n", newText) ;
+                printf("%s\n", newText);
                 //Borrar caracteres de una cadena
                 break;
             case 7:
@@ -212,44 +192,34 @@ void main()
             case 10:
                 //Validar correo electronico
 
-			printf("Ingrese su correo electronico\n>>");
+                printf("Ingrese su correo electronico\n>>");
 
-			gets(cadena);
-			gets(cadena);
+                gets(cadena);
+                gets(cadena);
 
-			for (int i = 0; i < 40; i++)
-			{
-				tok = strstr(cadena, "@");
-				if (tok != NULL)
-				{
-					break;
-				}
-			}
-			for (int i = 0; i < 40; i++)
-			{
-				next_tok1 = strstr(cadena, ".");
-				if (next_tok1 != NULL && &next_tok1 > &tok)
-				{
-					break;
-				}
-			}
+                for (int i = 0; i < 40; i++) {
+                    tok = strstr(cadena, "@");
+                    if (tok != NULL) {
+                        break;
+                    }
+                }
+                for (int i = 0; i < 40; i++) {
+                    next_tok1 = strstr(cadena, ".");
+                    if (next_tok1 != NULL && &next_tok1 > &tok) {
+                        break;
+                    }
+                }
 
-			if (tok != NULL)
-			{
-				if (next_tok1 != 0 && &next_tok1 > &tok)
-				{
-					printf("Correo valido.");
-				}
-				else
-				{
-					printf("Su correo es invalido, por favor reviselo.");
-				}
-			}
-			else
-			{
-				printf("Su correo es invalido, por favor reviselo.");
-			}
-		}
+                if (tok != NULL) {
+                    if (next_tok1 != 0 && &next_tok1 > &tok) {
+                        printf("Correo valido.");
+                    } else {
+                        printf("Su correo es invalido, por favor reviselo.");
+                    }
+                } else {
+                    printf("Su correo es invalido, por favor reviselo.");
+                }
+        }
 
-	} while (op != 11);
+    } while (op != 11);
 }
